ac243f7d70d85e52fbe87e86c711db03d3dd9d54
Xiaoqi Zhao <xiaoqi.zhao@volvo.com>
2025-08-31 22:12:10 +0800

    initialization

---
+++ Archi_Tutorial.mm
@@ -0,0 +1,129 @@
+<map version="freeplane 1.12.1">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="Archi Tutorial&#xa;(base on User Guide 5.6.0)" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1756648752130" VGAP_QUANTITY="3 pt"><hook NAME="MapStyle" background="#d6e8e8ff" zoom="0.826">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" fit_to_viewport="false" show_icons="BESIDE_NODES" associatedTemplateLocation="template:/light_sky_element_template.mm" show_tags="UNDER_NODES"/>
+    <tags category_separator="::"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_4172522" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#051552" BACKGROUND_COLOR="#5cd5e8" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.7 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#1164b0" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID" VGAP_QUANTITY="3 pt">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_4172522" STARTINCLINATION="116.25 pt;0 pt;" ENDINCLINATION="116.25 pt;28.5 pt;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="10" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#051552" WIDTH="2" DASH="SOLID"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" COLOR="#fff024" BACKGROUND_COLOR="#000000"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.tags">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#f6f9a1" TEXT_ALIGN="LEFT">
+<icon BUILTIN="clock2"/>
+<font SIZE="10" ITALIC="true"/>
+<edge COLOR="#000000"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#ffffff" BACKGROUND_COLOR="#cc7212" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#1164b0"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_1823054225" COLOR="#fff024" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#9d5e19">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#9d5e19" TRANSPARENCY="255" DESTINATION="ID_1823054225"/>
+<font SIZE="11" BOLD="true"/>
+<edge COLOR="#9d5e19"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.flower" COLOR="#ffffff" BACKGROUND_COLOR="#255aba" STYLE="oval" TEXT_ALIGN="CENTER" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="22 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f9d71c" BORDER_DASH_LIKE_EDGE="false" BORDER_DASH="CLOSE_DOTS" MAX_WIDTH="6 cm" MIN_WIDTH="3 cm"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffffff" BACKGROUND_COLOR="#053d8b" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#2c2b29" BORDER_DASH_LIKE_EDGE="true">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#ffffff" BACKGROUND_COLOR="#1164b0" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR="#2c2b29">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#ffffff" BACKGROUND_COLOR="#298bc8" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#ffffff" BACKGROUND_COLOR="#3fb7db" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#051552" BACKGROUND_COLOR="#5cd5e8" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" BORDER_COLOR="#f0f0f0">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11" BORDER_COLOR="#f0f0f0">
+<font SIZE="9"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<font BOLD="true"/>
+<node TEXT="01 Introduction" POSITION="bottom_or_right" ID="ID_927757062" CREATED="1756648717280" MODIFIED="1756648733978"/>
+<node TEXT="02 Installing and Launching Archi" POSITION="bottom_or_right" ID="ID_1592065611" CREATED="1756648734160" MODIFIED="1756648828930"/>
+<node TEXT="03 Working in Archi" POSITION="bottom_or_right" ID="ID_1804088701" CREATED="1756648829265" MODIFIED="1756648834043"/>
+<node TEXT="04 Creating a New ArchiMate Model" POSITION="bottom_or_right" ID="ID_667453537" CREATED="1756648834198" MODIFIED="1756648842539"/>
+<node TEXT="05 The Model Tree" POSITION="bottom_or_right" ID="ID_916062858" CREATED="1756648842720" MODIFIED="1756648847850"/>
+<node TEXT="06 Views in Archi" POSITION="bottom_or_right" ID="ID_304410743" CREATED="1756648848033" MODIFIED="1756648856635"/>
+<node TEXT="07 Viewpoints" POSITION="bottom_or_right" ID="ID_1410555996" CREATED="1756648861544" MODIFIED="1756648865161"/>
+<node TEXT="08 The Properties Windows" POSITION="bottom_or_right" ID="ID_1163929574" CREATED="1756648865488" MODIFIED="1756648873531"/>
+<node TEXT="09 The Navigator Windows" POSITION="bottom_or_right" ID="ID_1481451788" CREATED="1756648873704" MODIFIED="1756648880963"/>
+<node TEXT="10 The Visualiser Windows" POSITION="bottom_or_right" ID="ID_202584990" CREATED="1756648881135" MODIFIED="1756648888600"/>
+<node TEXT="11 The Hints Windows" POSITION="bottom_or_right" ID="ID_1405974383" CREATED="1756648888897" MODIFIED="1756648894546"/>
+<node TEXT="12 The Outline Windows" POSITION="bottom_or_right" ID="ID_1219746834" CREATED="1756648894719" MODIFIED="1756648901891"/>
+<node TEXT="13 Tools" POSITION="bottom_or_right" ID="ID_551744421" CREATED="1756648902111" MODIFIED="1756648906320"/>
+<node TEXT="14 Generate a View from Elements" POSITION="bottom_or_right" ID="ID_1428331489" CREATED="1756648906479" MODIFIED="1756648915975"/>
+<node TEXT="15 Properties Manager" POSITION="bottom_or_right" ID="ID_856519067" CREATED="1756648916375" MODIFIED="1756648922969"/>
+<node TEXT="16 Specializations Manager" POSITION="bottom_or_right" ID="ID_1558642240" CREATED="1756648923141" MODIFIED="1756648930489"/>
+<node TEXT="17 The Validator" POSITION="bottom_or_right" ID="ID_147061043" CREATED="1756648930655" MODIFIED="1756648936025"/>
+<node TEXT="18 Preview HTML Report" POSITION="bottom_or_right" ID="ID_1500481882" CREATED="1756648941200" MODIFIED="1756648947925"/>
+<node TEXT="19 Opening, Saving and Printing" POSITION="bottom_or_right" ID="ID_34498363" CREATED="1756648948079" MODIFIED="1756648953977"/>
+<node TEXT="20 Exporting and Importing the Model in CSV Format" POSITION="bottom_or_right" ID="ID_1406030821" CREATED="1756648954144" MODIFIED="1756648971674"/>
+<node TEXT="21 Importing Another Model into the Current Model" POSITION="top_or_left" ID="ID_1220619441" CREATED="1756648971839" MODIFIED="1756648980586"/>
+<node TEXT="22 The Open Group Exchange File Format" POSITION="top_or_left" ID="ID_230622080" CREATED="1756648980735" MODIFIED="1756648991865"/>
+<node TEXT="23 Reporting" POSITION="top_or_left" ID="ID_1221340711" CREATED="1756649029340" MODIFIED="1756649033711"/>
+<node TEXT="24 HTML Report" POSITION="top_or_left" ID="ID_89963823" CREATED="1756649033861" MODIFIED="1756649038517"/>
+<node TEXT="25 Jasper Report" POSITION="top_or_left" ID="ID_1438041269" CREATED="1756649038692" MODIFIED="1756649044809"/>
+<node TEXT="26 Templates" POSITION="top_or_left" ID="ID_1414367311" CREATED="1756649044972" MODIFIED="1756649049739"/>
+<node TEXT="27 Relationships in ArchiMate" POSITION="top_or_left" ID="ID_168198230" CREATED="1756649049878" MODIFIED="1756649058471"/>
+<node TEXT="28 The Sketch View" POSITION="top_or_left" ID="ID_651238319" CREATED="1756649058640" MODIFIED="1756649064977"/>
+<node TEXT="29 The Canvas Modelling Toolkit" POSITION="top_or_left" ID="ID_425707569" CREATED="1756649065144" MODIFIED="1756649078089"/>
+<node TEXT="30 Preferences" POSITION="top_or_left" ID="ID_1703148013" CREATED="1756649078717" MODIFIED="1756649085689"/>
+<node TEXT="31 Appearance, Colours, and Fonts Preferences" POSITION="top_or_left" ID="ID_1537399996" CREATED="1756649085861" MODIFIED="1756649103943"/>
+<node TEXT="32 Canvas Modelling Toolkit Preferences" POSITION="top_or_left" ID="ID_1327147858" CREATED="1756649104099" MODIFIED="1756649120407"/>
+<node TEXT="33 Connections Preferences" POSITION="top_or_left" ID="ID_934079597" CREATED="1756649120564" MODIFIED="1756649132716"/>
+<node TEXT="34 Diagram Preferences" POSITION="top_or_left" ID="ID_701346856" CREATED="1756649132988" MODIFIED="1756649140190"/>
+<node TEXT="35 General Preferences" POSITION="top_or_left" ID="ID_738057817" CREATED="1756649140356" MODIFIED="1756649150478"/>
+<node TEXT="36 Jasper Reports Preferences" POSITION="top_or_left" ID="ID_47935893" CREATED="1756649150676" MODIFIED="1756649158765"/>
+<node TEXT="37 System Preferences" POSITION="top_or_left" ID="ID_240020828" CREATED="1756649163893" MODIFIED="1756649169750"/>
+<node TEXT="38 Validator Preferences" POSITION="top_or_left" ID="ID_1380047855" CREATED="1756649169924" MODIFIED="1756649184910"/>
+<node TEXT="39 Managing Plug-ins" POSITION="top_or_left" ID="ID_1370846004" CREATED="1756649185100" MODIFIED="1756649194583"/>
+<node TEXT="40 Beyond the User Guide" POSITION="top_or_left" ID="ID_380810123" CREATED="1756649194731" MODIFIED="1756649202910"/>
+</node>
+</map>
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Archi_Tutorial
-Hands-on demos for Archi (ArchiMate Modeling) tool base on "Archi User Guide)
+
+This is the repository to document hands-on demos for Archi (ArchiMate Modeling) tool base on "Archi User Guide"
+
+- [Archi\_Tutorial](#archi_tutorial)
+
